5095ead3093397cebd36e81febaf763c50616ef6
Alex Foster <alex.foster@freshwatercreative.ca>
2017-04-13 11:24:33 -0700

    Adding folders

---
+++ spa_interview/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Song.create!(title: 'Airbag', artist: 'Radiohead')
+Song.create!(title: 'The Pot', artist: 'tool')
+Song.create!(title: 'Michigan', artist: 'Milk Carton Kids')
+Song.create!(title: 'Dashboard', artist: 'Modest Mouse')
+Song.create!(title: 'Lump', artist: 'The Presidents')
